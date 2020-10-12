532b28b3550c9b1ad2771ce5928c208eaa4aea47
Konstantin Kotov <truck01@mail.ru>
2020-10-12 08:41:59 +0300

    Configure cache component

--- config/web.php
+++ config/web.php
@@ -1,19 +1,20 @@
 <?php
 
+use app\assets\YiiAsset;
 use app\core\manage\auth\UserIdentity;
-use app\modules\kinozal\KinozalModule;
-use app\modules\audio\AudioModule;
 use app\modules\adminka\Module as Adminka;
+use app\modules\audio\AudioModule;
+use app\modules\kinozal\KinozalModule;
+use kartik\file\PiExifAsset;
+use yii\bootstrap\BootstrapAsset;
+use yii\bootstrap\BootstrapPluginAsset;
+use yii\caching\FileCache;
+use yii\grid\GridViewAsset;
 use yii\log\FileTarget;
 use yii\rbac\PhpManager;
-use yii\web\JqueryAsset;
-use yii\bootstrap\BootstrapPluginAsset;
-use yii\bootstrap\BootstrapAsset;
-use app\assets\YiiAsset;
-use kartik\file\PiExifAsset;
-use yii\grid\GridViewAsset;
-use yii\widgets\ActiveFormAsset;
 use yii\validators\ValidationAsset;
+use yii\web\JqueryAsset;
+use yii\widgets\ActiveFormAsset;
 
 $params = require_once __DIR__ . '/params.php';
     return [
@@ -84,6 +85,10 @@ $params = require_once __DIR__ . '/params.php';
                     'identityCookie' => ['name' => '_identity', 'httpOnly' => true, 'domain' => $params['cookieDomain']],
                     'loginUrl' => ['adminka/login'],
             ],
+            'cache' => [
+                'class' => FileCache::class,
+                'defaultDuration' => 86400
+            ],            
         ],
         'modules' => [   
             'kinozal' => [
